d96a9818fda2071c9ac5c730d797f1dfb69b4deb
Jeff Palm <jeff@jeffpalm.com>
2022-01-02 09:40:36 -0500

    update Sun Jan  2 09:40:36 EST 2022

--- foo/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/spudtrooper/foo
-
-go 1.17
-
-require github.com/spudtrooper/goutil v0.1.18
--- foo/scripts/commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-msg="$@"
-if [[ -z "$msg" ]]; then
-	msg="update $(date)"
-fi
-git add .
-git commit -m "$msg"
-open /Applications/GitHub\ Desktop.app
